13216d1cf82c1de07271d15828fb057d2a6042d5
SayemHosen890 <sayemhosen902@gmail.com>
2025-08-07 17:50:44 +0600

    first-commit

--- screen/GameScreen.js
+++ screen/GameScreen.js
@@ -1,6 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 import Title from "../components/Title";
 
+function generateRandomBetween(min,max,exclude){
+    const rndNum=Math.floor(Math.random()*(max-min))+min;
+
+    if(rndNum===exclude){
+        return generateRandomBetween(min,max,exclude);
+    } else {
+        return rndNum;
+    }
+}
+
 const GameScreen = () => {
   return (
     <View style={styles.screen}>
